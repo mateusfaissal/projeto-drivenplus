bc2ab28ff9a4ad07f33e92087a45029f0a7a18bb
Mateus Faissal <mateusfaissal@gmail.com>
2023-08-05 18:15:34 -0300

    second commit: installing library and routes configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import PlansPage from "./pages/PlansPage";
+import SubsPlanPage from "./pages/SubsPlanPage";
+import HomePage from "./pages/HomePage";
 
 export default function App() {
 
+  const [infosUser, setInfosUser] = useState({});
+
   return (
-    <h1> APP </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}></Route>
+        <Route path="/sign-up" element={<SignUpPage />}></Route>
+        <Route path="/subscriptions" element={<PlansPage />}></Route>
+        <Route path="/subscriptions/:idPlan" element={<SubsPlanPage />}></Route>
+        <Route path='/home' element={<HomePage />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 
 }
